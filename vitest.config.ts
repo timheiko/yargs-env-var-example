9112c033df31e1c09b70931b5bb5518f9eeeffb6
Tim Heiko <175552092+timheiko@users.noreply.github.com>
2025-07-03 20:58:13 +0200

    Switched from Jest to Vitest ``` % npm install vitest --save-dev % npm i unplugin-auto-import --save-dev % npm install node/no-unpublished-import --save-dev ```

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config'
+import AutoImport from 'unplugin-auto-import/vite'
+
+export default defineConfig({
+  test: {
+    include: ['./src/**/*.test.ts'],
+  },
+  plugins: [
+    AutoImport({
+      imports: ['vitest'],
+      dts: true, // generate TypeScript declaration
+    }),
+  ],
+})
